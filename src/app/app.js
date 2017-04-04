948e13eaf612e8c09a51552c6bf2c63203a92534
Saurabh Srivastava <ssrivastava@attune.gshapps.net>
2017-04-04 19:07:45 +0530

    initial commit to develop

---
+++ src/app/app.js
@@ -0,0 +1,24 @@
+define(appConfig.app_scripts, function (jquery,angular) {
+    'use strict';
+    var app= angular.module('mymain', appConfig.app_modules);
+    app.service('AuthenticationService',appservice.authenticationHttpService)
+    
+    .controller('AppCtrl', ['$rootScope', '$scope', '$http', '$state',
+                function ($rootScope, $scope, $http, $state) {
+                }
+                 ])
+	.config(['$stateProvider', '$urlRouterProvider', '$httpProvider', function($stateProvider, $urlRouterProvider, $httpProvider) {	
+		//$routeProvider.otherwise({redirectTo: '/login'});
+		
+$urlRouterProvider.otherwise("/login");
+$stateProvider.state('logins', {
+                        url: "/login",
+                        views: {
+                            'root': {
+						templateUrl: 'login/login.tpl.html'
+						}
+						}
+                        });
+	}]);
+	return app;
+});
---
+++ src/app/login/login_controller.js
@@ -0,0 +1,42 @@
+define(appConfig.login_scripts, function($,angular,cfpLoadingBar) {
+	angular.module('login', appConfig.login_modules)
+	.controller('LoginCtrl', ['$rootScope', '$scope', '$timeout','$http', '$state','cfpLoadingBar',
+                function ($rootScope, $scope, $timeout,$http, $state,cfpLoadingBar) {
+            $scope.login = function()
+  {
+			this.loginInfo={
+				
+				"userName":$scope.username,
+				"password":$scope.password
+				
+			};
+			cfpLoadingBar.start();
+
+
+
+		/*Webservice.loginWebservice(this.loginInfo).then(function (response) {
+			$timeout( function(){ 
+			cfpLoadingBar.complete();
+            $state.go('employeeList');
+           }, 3000, false);
+			
+			
+		}, function (errorMessage) {
+			$timeout( function(){ 
+			cfpLoadingBar.complete();
+            $scope.validationMessage = "Invalid User Id/Password ";
+           }, 3000, false);    
+                });*/
+		//$scope.validateuserinfo(this.loginInfo);
+		//$http.defaults.headers.post['Content-Type'] = 'application/json';
+		//$http.post("http://192.168.1.93:8080/HKWebservices/Rest/login",JSON.stringify(this.loginInfo)).success(function(response){
+		//	$state.go('report');
+		//	}).error(function(error){
+		//		alert("error "+JSON.stringify(error));
+		//	});*/
+		};
+            
+                }
+                 ]);
+});
+	
